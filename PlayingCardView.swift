411769ee947e34b8ad5e57158928b598d23eb2d0
Sneh Patel <psneh1026@gmail.com>
2020-03-27 23:48:16 -0400

    added layout and gestures

--- PlayingCardView.swift
+++ PlayingCardView.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
+@IBDesignable
 class PlayingCardView: UIView {
     
-    var rank: Int = 5 { didSet { setNeedsDisplay(); setNeedsLayout() }}
+    @IBInspectable
+    var rank: Int = 11 { didSet { setNeedsDisplay(); setNeedsLayout() }}
+    
+    @IBInspectable
     var suit: String = "♥️" { didSet { setNeedsDisplay(); setNeedsLayout() }}
+    
+    @IBInspectable 
     var isFacedUp = true { didSet { setNeedsDisplay(); setNeedsLayout() }}
-
+    
+    var faceCardScale: CGFloat = SizeRatio.faceCardImageSizeToBoundsSize {didSet{ setNeedsDisplay()}}
+    
+    
+    @objc func adjustFaceCardScale(byHandlingGestureRecognizedBy recognizer: UIPinchGestureRecognizer){
+        switch recognizer.state {
+        case .changed,.ended:
+            faceCardScale *=  recognizer.scale
+            recognizer.scale = 1.0
+        default:
+            break 
+        }
+    }
+    
+    
+    
+    
+    
     private func centerAttributedString (_ string: String, fontSize: CGFloat) -> NSAttributedString {
         var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
         font = UIFontMetrics   (forTextStyle: .body).scaledFont(for: font)
@@ -67,7 +90,48 @@ class PlayingCardView: UIView {
     }
     
     
-
+    private func drawPips()
+    {
+        let pipsPerRowForRank = [[0], [1], [1,1], [1,1,1], [2,2], [2,1,2], [2,2,2], [2,1,2,2], [2,2,2,2], [2,2,1,2,2], [2,2,2,2,2]]
+        
+        func createPipString(thatFits pipRect: CGRect) -> NSAttributedString {
+            let maxVerticalPipCount = CGFloat(pipsPerRowForRank.reduce(0) { max($1.count, $0)})
+            let maxHorizontalPipCount = CGFloat(pipsPerRowForRank.reduce(0) { max($1.max() ?? 0, $0)})
+            let verticalPipRowSpacing = pipRect.size.height / maxVerticalPipCount
+            let attemptedPipString = centerAttributedString(suit, fontSize: verticalPipRowSpacing)
+            let probablyOkayPipStringFontSize = verticalPipRowSpacing / (attemptedPipString.size().height / verticalPipRowSpacing)
+            let probablyOkayPipString = centerAttributedString(suit, fontSize: probablyOkayPipStringFontSize)
+            if probablyOkayPipString.size().width > pipRect.size.width / maxHorizontalPipCount {
+                return centerAttributedString(suit, fontSize: probablyOkayPipStringFontSize /
+                    (probablyOkayPipString.size().width / (pipRect.size.width / maxHorizontalPipCount)))
+            } else {
+                return probablyOkayPipString
+            }
+        }
+        
+        if pipsPerRowForRank.indices.contains(rank) {
+            let pipsPerRow = pipsPerRowForRank[rank]
+            var pipRect = bounds.insetBy(dx: cornerOffset, dy: cornerOffset).insetBy(dx: cornerString.size().width, dy: cornerString.size().height / 2)
+            let pipString = createPipString(thatFits: pipRect)
+            let pipRowSpacing = pipRect.size.height / CGFloat(pipsPerRow.count)
+            pipRect.size.height = pipString.size().height
+            pipRect.origin.y += (pipRowSpacing - pipRect.size.height) / 2
+            for pipCount in pipsPerRow {
+                switch pipCount {
+                case 1:
+                    pipString.draw(in: pipRect)
+                case 2:
+                    pipString.draw(in: pipRect.leftHalf)
+                    pipString.draw(in: pipRect.rightHalf)
+                default:
+                    break
+                }
+                pipRect.origin.y += pipRowSpacing
+            }
+        }
+    }
+    
+    
     override func draw(_ rect: CGRect) {
         
         let roundedRect = UIBezierPath( roundedRect: bounds, cornerRadius: cornerRadius)
@@ -75,9 +139,22 @@ class PlayingCardView: UIView {
         UIColor.white.setFill()
         roundedRect.fill()
         
+        if isFacedUp{
+            
+            if let faceCardImage = UIImage(named: rankString+suit, in: Bundle(for: self.classForCoder),compatibleWith: traitCollection){
+                faceCardImage.draw(in: bounds.zoom(by: faceCardScale))
+            } else {
+                drawPips()
+            }
+            
+        } else {
+            if let cardBackImage = UIImage(named: "cardback", in: Bundle(for: self.classForCoder),compatibleWith: traitCollection){
+                cardBackImage.draw(in: bounds.zoom(by: SizeRatio.faceDownCardImageSizeToBoundsSize))
+            }
+        }
     }
-
-
+    
+    
 }
 
 extension PlayingCardView {
@@ -86,6 +163,7 @@ extension PlayingCardView {
         static let cornerRadiusToBoundsHeight: CGFloat = 0.06
         static let cornerOffsetToCornerRadius: CGFloat = 0.33
         static let faceCardImageSizeToBoundsSize: CGFloat = 0.75
+        static let faceDownCardImageSizeToBoundsSize: CGFloat = 0.4
     }
     
     private var cornerRadius: CGFloat {
--- ViewController.swift
+++ ViewController.swift
@@ -9,21 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var  deck = PlayingCardDeck()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        for _ in 1...10{
-            if let card = deck.draw() {
-                print("\(card)")
-            }
+    
+    @IBOutlet weak var playingCardView: PlayingCardView!{
+        didSet{
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [ .left, .right]
+            playingCardView.addGestureRecognizer(swipe)
             
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action:
+                #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pinch)
+        }
+    }
+    
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+                playingCardView.isFacedUp = !playingCardView.isFacedUp
+        default: break
         }
         
     }
-
-
+    
+    
+    @objc func nextCard() {
+        if let card = deck.draw(){
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue  
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
 }
+
+
+
 
